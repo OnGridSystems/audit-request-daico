d80fc2d20b2cc212c7918e84eba220d1ea7468dd
Dmitry Romanov <d.romanov@ongrid.pro>
2019-02-12 15:28:56 +0300

    NT-30
    Add ProxyClaimable contract
    Add ProxyTransferable
    Add tests for new contracts

---
+++ contracts/ProxyClaimable.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.0;
+
+import "./Claimable.sol";
+
+
+/**
+ * @title Claimable
+ * @dev Claimable contract, where the ownership needs to be claimed.
+ * This allows the new owner to accept the transfer.
+ */
+contract ProxyClaimable {
+  address public owner;
+
+  /**
+  * @dev The ProxyClaimable constructor sets the original `owner` of the contract to the sender
+  * account.
+  */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+  * @dev Throws if called by any account other than the owner.
+  */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+  * @dev Call claimOwnership of another contract.
+  * @param _contractAddr The address of contract who must claim ownership.
+  */
+  function proxyClaimOwnership(address _contractAddr) external onlyOwner {
+    Claimable instance = Claimable(_contractAddr);
+    instance.claimOwnership();
+  }
+}
